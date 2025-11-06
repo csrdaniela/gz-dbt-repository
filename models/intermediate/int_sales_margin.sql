1139f9f9a388272aadcbfd4ec8ddc929a235de04
Daniela Sanchez <csrdaniela@gmail.com>
2025-11-06 15:35:15 +0000

    macros

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -7,6 +7,7 @@ select
   , purchase_cost
   , purchase_cost * quantity as total_purchase_cost
   , round( revenue - (purchase_cost * quantity),2) as margin
+  , {{margin_percent ( 'revenue', 'purchase_cost' ) }} as margin_percent
 from 
   {{ ref('stg_gz_raw_data__sales') }}
 join 
