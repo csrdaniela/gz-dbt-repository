642851dd911fe15aa04aef8d2fe61973b284e6ba
Daniela Sanchez <csrdaniela@gmail.com>
2025-11-06 10:07:43 +0000

    operational margin per order and other columns too

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,9 +1,16 @@
 select
-  orders_id
-  , date_date
-  , round((margin + s.shipping_fee - s.logcost - s.ship_cost),2) as operational_margin
+  o.orders_id
+  , o.date_date
+  , o.revenue
+  , o.quantity
+  , o.purchase_cost
+  , o.margin
+  , round((o.margin + s.shipping_fee - s.logcost - s.ship_cost),2) as operational_margin
+  , s.shipping_fee
+  , s.logcost
+  , s.ship_cost
 from 
-{{ ref('int_orders_margin') }}
+{{ ref('int_orders_margin') }} as o
 join
 {{ ref('stg_gz_raw_data__ship') }} as s
 using (orders_id)
